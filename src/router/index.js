6702e53fddf16ca89f876eb84fb7f3e434fc0b8a
ccumaco <carlos.cumaco@inoutdelivery.com>
2020-11-04 17:57:28 -0500

    php y route

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
-
-  const routes = [
+Vue.use(Router)
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+ routes: [
   {
     path: '/',
     name: 'Home',
@@ -61,10 +63,5 @@ Vue.use(VueRouter)
   }
 ]
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
 
-export default router
+})
